a87830993abf5b1df267b661be8a5be89f959f88
Harteos7 <113110274+Harteos7@users.noreply.github.com>
2022-12-19 13:18:04 +0100

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var routes = <String, WidgetBuilder>{
+      MyItemsPage.routeName: (BuildContext context) => new MyItemsPage(title: "MyItemsPage"),
+      AwesomeButton.routeName: (BuildContext context) => new AwesomeButton(),
+    };
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      routes: routes,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({required this.title}) ;
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    Navigator.pushNamed(context, MyItemsPage.routeName);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var button = new IconButton(icon: new Icon(Icons.access_alarm), onPressed: _onButtonPressed1);
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new Column(
+        children: <Widget>[
+          new Text('Dog'),
+          new Text('Cat'),
+          button
+        ],
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: new Icon(Icons.add),
+      ),
+    );
+  }
+
+  void _onButtonPressed() {
+    Navigator.pushNamed(context, MyItemsPage.routeName);
+  }
+
+  void _onButtonPressed1() {
+    Navigator.pushNamed(context, AwesomeButton.routeName);
+  }
+}
+
+class MyItemsPage extends StatefulWidget {
+  MyItemsPage({required this.title}) ;
+
+  static const String routeName = "/MyItemsPage";
+
+  final String title;
+
+  @override
+  _MyItemsPageState createState() => new _MyItemsPageState();
+}
+
+/// // 1. After the page has been created, register it with the app routes
+/// routes: <String, WidgetBuilder>{
+///   MyItemsPage.routeName: (BuildContext context) => new MyItemsPage(title: "MyItemsPage"),
+/// },
+///
+/// // 2. Then this could be used to navigate to the page.
+/// Navigator.pushNamed(context, MyItemsPage.routeName);
+///
+
+class _MyItemsPageState extends State<MyItemsPage> {
+  @override
+  Widget build(BuildContext context) {
+    var button = new IconButton(icon: new Icon(Icons.arrow_back), onPressed: _onButtonPressed);
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new Container(
+        child: new Column(
+          children: <Widget>[
+            new Text('Item1'),
+            new Text('Item2'),
+            button
+          ],
+        ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _onFloatingActionButtonPressed,
+        tooltip: 'Add',
+        child: new Icon(Icons.add),
+      ),
+    );
+  }
+
+  void _onFloatingActionButtonPressed() {
+  }
+
+  void _onButtonPressed() {
+    Navigator.pop(context);
+  }
+}
+
+
+class AwesomeButton extends StatefulWidget {
+  @override
+  AwesomeButtonState createState() => new AwesomeButtonState();
+  static const String routeName = "/AwesomeButton";
+}
+
+class AwesomeButtonState extends State<AwesomeButton> {
+
+  int counter = 0;
+  List<String> strings = ["Flutter", "Is", "Awesome","I","Play","Pokemon","Go","Every","Day"];
+  String displayedString = "";
+
+  //you can also declare variables like this
+  // var counter = 0;
+  // var strings = ["Flutter", "Is", "Awesome"];
+
+  void onPressed(){
+    setState(() {
+      displayedString = strings[counter];
+      counter = counter < 8 ? counter+1 : 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("Stateful Widget!"), backgroundColor: Colors.deepOrange),
+      body: new Container(
+        child: new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Text(displayedString, style: new TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold)),
+              new Padding(padding: new EdgeInsets.all(15.0)),
+              new ElevatedButton(
+                child: new Text("Press me!", style: new TextStyle(color: Colors.white, fontStyle: FontStyle.italic, fontSize: 20.0)),
+                style: ElevatedButton.styleFrom(
+                primary: Colors.purple),
+                onPressed: onPressed
+              )
+            ]
+          )
+        )
+      )
+    );
+  }
+}
