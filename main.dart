bb29b78bd1b7828a4dea36cb704fd58321b80ada
Harteos7 <113110274+Harteos7@users.noreply.github.com>
2023-02-24 17:12:54 +0100

    main.dart for snake app

--- main.dart
+++ main.dart
@@ -1,170 +1,256 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+import 'dart:async';
+import 'package:flutter/services.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var routes = <String, WidgetBuilder>{
-      MyItemsPage.routeName: (BuildContext context) => new MyItemsPage(title: "MyItemsPage"),
-      AwesomeButton.routeName: (BuildContext context) => new AwesomeButton(),
-    };
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
-      routes: routes,
+    return MaterialApp(
+      home: SnakeGame(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({required this.title}) ;
-
-  final String title;
-
+class SnakeGame extends StatefulWidget {
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  _SnakeGameState createState() => _SnakeGameState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _SnakeGameState extends State<SnakeGame> {
+  final int squaresPerRow = 20;
+  final int squaresPerCol = 40;
+  final fontStyle = TextStyle(color: Colors.white, fontSize: 20);
+  final randomGen = Random();
 
-  void _incrementCounter() {
-    Navigator.pushNamed(context, MyItemsPage.routeName);
-  }
+  var snake = [
+    [0, 1],
+    [0, 0]
+  ];
+  var food = [0, 2];
+  var direction = 'up';
+  var isPlaying = false;
 
+    // The node used to request the keyboard focus.
+  final FocusNode _focusNode = FocusNode();
+  // The message to display.
+  String? _message;
+  // key for snake
+
+  // Focus nodes need to be disposed.
   @override
-  Widget build(BuildContext context) {
-    var button = new IconButton(icon: new Icon(Icons.access_alarm), onPressed: _onButtonPressed1);
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
-      body: new Column(
-        children: <Widget>[
-          new Text('Dog'),
-          new Text('Cat'),
-          button
-        ],
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ),
-    );
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
   }
 
-  void _onButtonPressed() {
-    Navigator.pushNamed(context, MyItemsPage.routeName);
-  }
-
-  void _onButtonPressed1() {
-    Navigator.pushNamed(context, AwesomeButton.routeName);
-  }
-}
-
-class MyItemsPage extends StatefulWidget {
-  MyItemsPage({required this.title}) ;
-
-  static const String routeName = "/MyItemsPage";
-
-  final String title;
-
-  @override
-  _MyItemsPageState createState() => new _MyItemsPageState();
-}
-
-/// // 1. After the page has been created, register it with the app routes
-/// routes: <String, WidgetBuilder>{
-///   MyItemsPage.routeName: (BuildContext context) => new MyItemsPage(title: "MyItemsPage"),
-/// },
-///
-/// // 2. Then this could be used to navigate to the page.
-/// Navigator.pushNamed(context, MyItemsPage.routeName);
-///
-
-class _MyItemsPageState extends State<MyItemsPage> {
-  @override
-  Widget build(BuildContext context) {
-    var button = new IconButton(icon: new Icon(Icons.arrow_back), onPressed: _onButtonPressed);
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
-      body: new Container(
-        child: new Column(
-          children: <Widget>[
-            new Text('Item1'),
-            new Text('Item2'),
-            button
-          ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _onFloatingActionButtonPressed,
-        tooltip: 'Add',
-        child: new Icon(Icons.add),
-      ),
-    );
-  }
-
-  void _onFloatingActionButtonPressed() {
-  }
-
-  void _onButtonPressed() {
-    Navigator.pop(context);
-  }
-}
-
-
-class AwesomeButton extends StatefulWidget {
-  @override
-  AwesomeButtonState createState() => new AwesomeButtonState();
-  static const String routeName = "/AwesomeButton";
-}
-
-class AwesomeButtonState extends State<AwesomeButton> {
-
-  int counter = 0;
-  List<String> strings = ["Flutter", "Is", "Awesome","I","Play","Pokemon","Go","Every","Day"];
-  String displayedString = "";
-
-  //you can also declare variables like this
-  // var counter = 0;
-  // var strings = ["Flutter", "Is", "Awesome"];
-
-  void onPressed(){
+  // Handles the key events from the Focus widget and updates the
+  // _message.
+  KeyEventResult _handleKeyEvent(FocusNode node, RawKeyEvent event) {
     setState(() {
-      displayedString = strings[counter];
-      counter = counter < 8 ? counter+1 : 0;
+      _message = '${event.logicalKey.debugName}';
+      if (event.logicalKey == LogicalKeyboardKey.keyS) {
+        direction = 'down';
+      }
+      if (event.logicalKey == LogicalKeyboardKey.keyZ) {
+        direction = 'up';
+      }
+      if (event.logicalKey == LogicalKeyboardKey.keyD) {
+        direction = 'right';
+      }
+      if (event.logicalKey == LogicalKeyboardKey.keyS) {
+        direction = 'left';
+      }
+      switch(direction) {
+        case 'up':
+          snake.insert(0, [snake.first[0], snake.first[1] - 1]);
+          break;
+        
+        case 'down':
+          snake.insert(0, [snake.first[0], snake.first[1] + 1]);
+          break;
+
+        case 'left':
+          snake.insert(0, [snake.first[0] - 1, snake.first[1]]);
+          break;
+
+        case 'right':
+          snake.insert(0, [snake.first[0] + 1, snake.first[1]]);
+          break;
+      }
+
+      if (snake.first[0] != food[0] || snake.first[1] != food[1]) {
+        snake.removeLast();
+      } else {
+        createFood();
+      }
+    });
+    return event.logicalKey == LogicalKeyboardKey.keyP
+        ? KeyEventResult.handled
+        : KeyEventResult.ignored;
+  }
+
+  void startGame() {
+    const duration = Duration(milliseconds: 300);
+
+    snake = [ // Snake head
+      [(squaresPerRow / 2).floor(), (squaresPerCol / 2).floor()]
+    ];
+
+    snake.add([snake.first[0], snake.first[1]+1]); // Snake body
+
+    createFood();
+
+    isPlaying = true;
+    Timer.periodic(duration, (Timer timer) { 
+      moveSnake();
+      if (checkGameOver()) {
+        timer.cancel();
+        endGame();
+      }
+    });
+  }
+  
+  void moveSnake() {
+    setState(() {
+
     });
   }
 
-  @override
-  Widget build(BuildContext context){
-    return new Scaffold(
-      appBar: new AppBar(title: new Text("Stateful Widget!"), backgroundColor: Colors.deepOrange),
-      body: new Container(
-        child: new Center(
-          child: new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              new Text(displayedString, style: new TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold)),
-              new Padding(padding: new EdgeInsets.all(15.0)),
-              new ElevatedButton(
-                child: new Text("Press me!", style: new TextStyle(color: Colors.white, fontStyle: FontStyle.italic, fontSize: 20.0)),
-                style: ElevatedButton.styleFrom(
-                primary: Colors.purple),
-                onPressed: onPressed
-              )
-            ]
-          )
-        )
-      )
+  void createFood() {
+    food = [
+      randomGen.nextInt(squaresPerRow),
+      randomGen.nextInt(squaresPerCol)
+    ];
+  }
+
+  bool checkGameOver() {
+    if (!isPlaying
+      || snake.first[1] < 0
+      || snake.first[1] >= squaresPerCol
+      || snake.first[0] < 0
+      || snake.first[0] > squaresPerRow
+    ) {
+      return true;
+    }
+
+    for(var i=1; i < snake.length; ++i) {
+      if (snake[i][0] == snake.first[0] && snake[i][1] == snake.first[1]) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  void endGame() {
+    isPlaying = false;
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Game Over'),
+          content: Text(
+            'Score: ${snake.length - 2}',
+            style: TextStyle(fontSize: 20),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Close'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      }
+    );
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: GestureDetector(
+              child: Focus(
+                onKey: _handleKeyEvent,
+                child: AspectRatio(
+                  aspectRatio: squaresPerRow / (squaresPerCol + 5),
+                  child: GridView.builder(
+                      physics: const NeverScrollableScrollPhysics(),
+                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: squaresPerRow,
+                      ),
+                      itemCount: squaresPerRow * squaresPerCol,
+                      itemBuilder: (BuildContext context, int index) {
+                        var color;
+                        var x = index % squaresPerRow;
+                        var y = (index / squaresPerRow).floor();
+
+                        bool isSnakeBody = false;
+                        for (var pos in snake) {
+                          if (pos[0] == x && pos[1] == y) {
+                            isSnakeBody = true;
+                            break;
+                          }
+                        }
+
+                        if (snake.first[0] == x && snake.first[1] == y) {
+                          color = Colors.green;
+                        } else if (isSnakeBody) {
+                          color = Colors.green[200];
+                        } else if (food[0] == x && food[1] == y) {
+                          color = Colors.red;
+                        } else {
+                          color = Colors.grey[800];
+                        }
+
+                        return Container(
+                          margin: EdgeInsets.all(1),
+                          decoration: BoxDecoration(
+                            color: color,
+                            shape: BoxShape.circle,
+                          ),
+                        );
+                      }),
+                ),
+              ),
+          
+            ),
+          ),
+          Padding(
+              padding: EdgeInsets.only(bottom: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  TextButton(
+                      style: TextButton.styleFrom(
+                        foregroundColor: isPlaying ? Colors.red : Colors.blue,
+                      ), // 
+                      child: Text(
+                        isPlaying ? 'End' : 'Start',
+                        style: fontStyle,
+                      ),
+                      onPressed: () {
+                        if (isPlaying) {
+                          isPlaying = false;
+                        } else {
+                          startGame();
+                        }
+                      }),
+                  Text(
+                    'Score: ${snake.length - 2}',
+                    style: fontStyle,
+                  ),
+                ],
+              )),
+        ],
+      ),
     );
   }
 }
